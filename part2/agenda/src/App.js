c3c2ae0dd2a5f94699264429e55e49bbdab78259
Gaston Capdevila <gaston.capdevila118@gmail.com>
2023-12-05 17:28:45 -0300

    ejercicio2.9

--- part2/agenda/src/App.js
+++ part2/agenda/src/App.js
@@ -5,6 +5,7 @@ const App = () => {
   const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ newFilter, setNewFilter ] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -36,10 +37,20 @@ const handleNumberChange = (event) => {
   setNewNumber(event.target.value)
 }
 
+const handleSearchChange = (event) => {
+  setNewFilter(event.target.value);
+}
+
+const filteredPersons = persons.filter(person =>
+  person.content.toLowerCase().includes(newFilter.toLowerCase())
+);
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        search: <input value={newFilter} onChange={handleSearchChange} />
+      </div>
       <form onSubmit={addName}>
         <div>name: <input value={newName} onChange={handlePersonChange} required/></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange} required/></div>
@@ -47,7 +58,7 @@ const handleNumberChange = (event) => {
       </form>
       <h2>Numbers</h2>
       <ul>
-       {persons.map((person) => (
+       {filteredPersons.map((person) => (
         <Person key={person.id} person={person} />
       ))}
       </ul>
