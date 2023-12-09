1f6584cb0a9062542e8fcdbfec2916810a51a7bb
Gaston Capdevila <gaston.capdevila118@gmail.com>
2023-12-09 19:26:09 -0300

    3.10

--- part2/agenda/src/services/persons.js
+++ part2/agenda/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://api-persons.onrender.com/api/persons'
 
 const getAll = () => {
   return axios.get(baseUrl)
