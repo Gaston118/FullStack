915f183f4a86839a9864838c7fa73e803f50b328
Gaston Capdevila <gaston.capdevila118@gmail.com>
2023-12-11 19:12:48 -0300

    4.5

--- part4/blogs/utils/list_helper.js
+++ part4/blogs/utils/list_helper.js
@@ -6,9 +6,28 @@ const dummy = (blogs) => {
   const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0);
   }
+
+  const favoriteBlog = (blogs) => {
+    // Verificar si la lista de blogs está vacía
+    if (blogs.length === 0) {
+      return null; // Devolver null si no hay blogs
+    }
+  
+    // Utilizar reduce para encontrar el blog con más likes
+    const favorite = blogs.reduce((maxLikesBlog, currentBlog) => {
+      return currentBlog.likes > maxLikesBlog.likes ? currentBlog : maxLikesBlog;
+    }, blogs[0]); // Inicializar con el primer blog
+  
+    return {
+      title: favorite.title,
+      author: favorite.author,
+      likes: favorite.likes,
+    };
+  }
   
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   };
   
