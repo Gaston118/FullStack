a6a3eb6163e968b1bda611cbc347d7dbf4fb4e79
Gaston Capdevila <gaston.capdevila118@gmail.com>
2023-12-22 09:25:23 -0300

    6.1.1

---
+++ part6/part6.1/src/App.js
@@ -0,0 +1,13 @@
+import Notes from "./components/Note"
+import NewNote from "./components/NewNote"
+
+const App = () => {
+  return(
+    <div>
+      <NewNote />
+      <Notes />
+    </div>
+  )
+}
+
+export default App
