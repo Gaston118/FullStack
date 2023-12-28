9cefde137de60dc707deafe2735b26c8d84b9d7f
Gaston Capdevila <gaston.capdevila118@gmail.com>
2023-12-28 11:17:32 -0300

    retoques

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -2,13 +2,16 @@ import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 import { getAnecdotes, updateAnecdote } from './components/request'
 import { useQuery, useMutation, useQueryClient } from 'react-query'
+import { useNotify } from './NotificationContext'
 
 const App = () => {
   const queryClient = useQueryClient()
+  const notifyWith = useNotify()
 
   const updateAnecdoteMutation = useMutation(updateAnecdote, {
-    onSuccess: () => {
+    onSuccess: ({content}) => {
       queryClient.invalidateQueries('anecdotes')
+      notifyWith(`anecdote '${content}' voted`)
     },
   })
 
--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,15 +1,20 @@
 import { getAnecdotes, createAnecdote, updateAnecdote } from './request'
 import { useQuery, useMutation, useQueryClient } from 'react-query'
+import { useNotify } from '../NotificationContext'
 
 const AnecdoteForm = () => {
-
+  const notifyWith = useNotify()
   const queryClient = useQueryClient() //LO QUE HACE ESTO ES QUE RECUPERA LAS NOTAS DEL SEV DESPUES DE 
   //HABERLA AGREGADO.
   const newAnecdoteMutation = useMutation(createAnecdote, {
     onSuccess: (newAnecdote) => {
       const anecdotes = queryClient.getQueryData('anecdotes')
       queryClient.setQueryData('anecdotes', anecdotes.concat(newAnecdote))
+      notifyWith(`anecdote '${newAnecdote.content}' created`)
     },
+    onError: (error) => {
+      notifyWith(error.response.data.error)
+    }
   })
 
   const onCreate = (event) => {
